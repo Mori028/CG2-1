ded3e40aa2aa8c2d306ebd4ea013c19339842916
Moriatsushi <k021g1463@g.neec.ac.jp>
2022-04-13 09:38:35 +0900

    CG2 01_01

--- main.cpp
+++ main.cpp
@@ -1,57 +1,77 @@
 #include <Windows.h>
+//ウィンドウプロシージャ
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+    //メッセージに応じてゲーム固有の処理を行う
+    switch (msg) {
+        //ウィンドウが破棄された
+    case WM_DESTROY:
+        //osに対して、アプリの終了を伝える
+        PostQuitMessage(0);
+        return 0;
+    }
+    //標準メッセージ処理を行う
+    return DefWindowProc(hwnd, msg, wparam, lparam);
+}
 
 //windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	//コンソールへの文字出力
-	OutputDebugStringA("Hellow,DirectX!!\n");
+    //コンソールへの文字出力
+    OutputDebugStringA("Hellow,DirectX!!\n");
+    //ウィンドウサイズ
+// ウィンドウ横幅
+    const int WIN_WIDTH = 1280;
+    // ウィンドウ縦幅
+    const int WIN_HEIGHT = 720;
 
-	return 0;
+    //ウィンドウクラスの設定
+    WNDCLASSEX w{};
+    w.cbSize = sizeof(WNDCLASSEX);
+    w.lpfnWndProc = (WNDPROC)WindowProc;//ウィンドウプロシージャを設定
+    w.lpszClassName = L"DirectXGame";//ウィンドクラス名
+    w.hInstance = GetModuleHandle(nullptr);//ウィンドハンドル
+    w.hCursor = LoadCursor(NULL, IDC_ARROW);//カーソル指定
+
+    //ウィンドクラスを05に登録する
+    RegisterClassEx(&w);
+    //ウィンドサイズ{X座標　Y座標　横幅　縦幅}
+    RECT wrc = { 0, 0, WIN_WIDTH, WIN_HEIGHT };
+    //自動でサイズを補正する
+    AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+    //ウィンドウオブジェクトの生成
+    HWND hwnd = CreateWindow(w.lpszClassName,//クラス名
+        L"DirectXGame",//タイトルバーの文字
+        WS_OVERLAPPEDWINDOW,//標準的なウィンドウスタイル
+        CW_USEDEFAULT,//標準X座標 (05に任せる)
+        CW_USEDEFAULT,//標準Y座標 (05に任せる)
+        wrc.right - wrc.left,//ウィンドウ横幅
+        wrc.bottom - wrc.top,//ウィンドウ縦幅
+        nullptr,
+        nullptr,
+        w.hInstance,
+        nullptr);
+
+    //ウィンドウを表示状態にする
+    ShowWindow(hwnd, SW_SHOW);
+    return 0;
 }
-	//ウィンドウプロシージャ
-LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-	//メッセージに応じてゲーム固有の処理を行う
-	switch (msg) {
-		//ウィンドウが破棄された
-	case WM_DESTROY:
-		//osに対して、アプリの終了を伝える
-		PostQuitMessage(0);
-		return 0;
-	}
-	//標準メッセージ処理を行う
-	return DefWindowProc(hwnd, msg, wparam, lparam);
+
+MSG msg{};//メッセージ
+//初期化処理 ここから
+
+//初期化処理　ここまで
+
+//ゲームループ
+while(true) {
+    //メッセージがある
+    if (peekMessage(6msg, nullptr, 0, 0, PM_REMOVE)) {
+        TranslateMessage(&msg);//キー入力メッセージの処理
+        DispatchMessage(&msg);//プロシージャにメッセージを送る
+    }
+    //Xボタンで終了メッセージが来たらゲームループを抜ける
+    if (msg.message == WM_QUIT) {
+        break;
+    }
 }
-//ウィンドウサイズ
-const int window_widht = 1280;//横幅
-const int window_height = 720;//縦幅
-//ウィンドウクラスの設定
-WNDCLASSEX w{};
-w.cbSize = sizeof(WNDCLASSEX);//ウィンドウプロシージャを設定
-w.lpfnWndProc = (WNDPROC)WindowProc;//ウィンドウクラス名
-w.lpszClassName = L"DirectXGame";//ウィンドウハンドル
-w.hInstance = GetModuleHandle(nullptr);//カーソル指定
-w.hCursor = LoadCursor(NULL, IDC_ARROW);
 
-//ウィンドウクラスをOSに登録する
-RegisterClassEx(&w);
-//ウィンドウサイズ{　X座標　Y座標　横幅　縦幅　}
-RECT wrc = { 0,0,window_width,window_height };
-//自動でサイズを補正する
-AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
-
-//ウィンドウオブジェクトの生成
-HWND hwnd = CreateWindow(w.lpszClassName,//クラス名
-	L"DirectXGame",
-	WS_OVERLAPPEDWINDOW,
-	CW_USEDEFAULT,
-	CW_USEDEFAULT,
-	wrc.right - wrc.left,
-	wrc.bottom - wrc.top,
-	nullptr,
-	nullptr,
-	w.hInstance,
-	nullptr);
-
-//ウィンドウを表示状態にする
-ShowWindow(hwnd, SW_SHOW);
-
-
+UnregisterClass(w.lpszClassName, w.hinstance);
